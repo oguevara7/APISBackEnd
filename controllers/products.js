e7d48838df50c0ac87cf9b9a6e19940fe5c09770
oguevara7 <38701556+oguevara7@users.noreply.github.com>
2019-05-09 03:25:03 -0500

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -17,6 +17,27 @@ const getProducts = function(req, res) {
   })
 }
 
+const updateProduct = function(req, res) {
+  const _id = req.params.id
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'description', 'photo']
+  const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+
+  if( !isValidUpdate ) {
+    return res.status(400).send({
+      error: 'Invalid update, only allowed to update: ' + allowedUpdates
+    })
+  }
+  Product.findByIdAndUpdate(_id, req.body ).then(function(char) {
+    if (!char) {
+      return res.status(404).send()
+    }
+    return res.send(char)
+  }).catch(function(error) {
+    res.status(500).send(error)
+  })
+}
+
 const deleteProduct = function(req, res) {
   const _id = req.params.id
   Product.findByIdAndDelete(_id).then(function(pro){
@@ -32,5 +53,6 @@ const deleteProduct = function(req, res) {
 module.exports = {
   createProduct: createProduct,
   getProducts: getProducts,
+  updateProduct: updateProduct,
   deleteProduct: deleteProduct
 }
